8aa52f0b48af688414d43bf4aeb536f2f7cbc205
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2016-04-26 09:33:24 +0200

    factory: separated factory for celery

--- cds/factory.py
+++ cds/factory.py
@@ -39,9 +39,29 @@ conf_loader = create_conf_loader(config=config, env_prefix=env_prefix)
 
 instance_path = os.getenv(env_prefix + '_INSTANCE_PATH') or \
     os.path.join(sys.prefix, 'var', 'instance')
+"""Path to instance folder.
+
+Defaults to ``<virtualenv>/var/instance/``. Can be overwritten using the
+environment variable ``APP_INSTANCE_PATH``.
+"""
 
 static_folder = os.getenv(env_prefix + '_STATIC_FOLDER') or \
     os.path.join(instance_path, 'static')
+"""Path to static folder.
+
+Defaults to ``<virtualenv>/var/instance/static/``. Can be overwritten
+using the environment variable ``APP_STATIC_FOLDER``
+"""
+
+create_celery = create_app_factory(
+    'cds',
+    config_loader=conf_loader,
+    extension_entry_points=['invenio_base.apps'],
+    blueprint_entry_points=['invenio_base.blueprints'],
+    instance_path=instance_path,
+    static_folder=static_folder,
+)
+"""Create CLI/Celery application."""
 
 create_api = create_app_factory(
     'cds',
@@ -50,7 +70,7 @@ create_api = create_app_factory(
     extension_entry_points=['invenio_base.api_apps'],
     instance_path=instance_path,
 )
-
+"""Create Flask API application."""
 
 create_app = create_app_factory(
     'cds',
@@ -61,3 +81,4 @@ create_app = create_app_factory(
     instance_path=instance_path,
     static_folder=static_folder,
 )
+"""Create Flask UI application."""
