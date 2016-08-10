fbd1b16844106f8ed4d9f78cbc953ec2e599fbe0
Harris Tzovanakis <me@drjova.com>
2016-08-10 17:31:40 +0200

    global: deposit initialization
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- cds/modules/deposit/views.py
+++ cds/modules/deposit/views.py
@@ -1,7 +1,7 @@
-{# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2016 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -21,21 +21,16 @@
 # In applying this license, CERN does not
 # waive the privileges and immunities granted to it by virtue of its status
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
-#}
-{%- extends "invenio_accounts/login_user.html" %}
 
-{%- block form_header %}
-  <h3 class="text-center panel-free-title">{{_('Log In') }}</h3>
-{%- endblock form_header %}
+"""CDS interface."""
 
-{%- block form_outer %}
-  {%- if config.DEBUG -%}
-    {{ super() }}
-  {%- endif -%}
-{%- endblock form_outer %}
+from __future__ import absolute_import, print_function
 
-{%- block page_body -%}
-  <div class="cds-login">
-    {{ super() }}
-  </div>
-{%- endblock -%}
+from flask import Blueprint
+
+blueprint = Blueprint(
+    'cds_deposit',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
