13159aa895ac3c08dc69807d78c59e9a79bfd3d6
Harris Tzovanakis <me@drjova.com>
2016-08-10 17:31:40 +0200

    global: deposit initialization
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- cds/factory.py
+++ cds/factory.py
@@ -58,6 +58,7 @@ create_celery = create_app_factory(
     config_loader=conf_loader,
     extension_entry_points=['invenio_base.apps'],
     blueprint_entry_points=['invenio_base.blueprints'],
+    converter_entry_points=['invenio_base.converters'],
     instance_path=instance_path,
     static_folder=static_folder,
 )
@@ -68,6 +69,7 @@ create_api = create_app_factory(
     config_loader=conf_loader,
     blueprint_entry_points=['invenio_base.api_blueprints'],
     extension_entry_points=['invenio_base.api_apps'],
+    converter_entry_points=['invenio_base.api_converters'],
     instance_path=instance_path,
 )
 """Create Flask API application."""
@@ -77,6 +79,7 @@ create_app = create_app_factory(
     config_loader=conf_loader,
     blueprint_entry_points=['invenio_base.blueprints'],
     extension_entry_points=['invenio_base.apps'],
+    converter_entry_points=['invenio_base.api_converters'],
     wsgi_factory=create_wsgi_factory({'/api': create_api}),
     instance_path=instance_path,
     static_folder=static_folder,
