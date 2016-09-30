eadf89dc49f31909f2b5d6cbef9acabea19e8205
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2016-09-30 10:52:31 +0200

    tests: test coverage increased
    * Adds test coverage
    
    * Updates the Flask-WTF pinned requirement
    
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- tests/unit/test_deposit.py
+++ tests/unit/test_deposit.py
@@ -27,13 +27,19 @@
 from __future__ import absolute_import, print_function
 
 import mock
-from flask import current_app, request, url_for
+from flask import current_app, g, request, url_for
+from flask_login import login_user
+from flask_principal import Identity
+from invenio_accounts.models import User
+from invenio_db import db
 from invenio_files_rest.models import Bucket
 from invenio_records.models import RecordMetadata
 from invenio_records_files.api import RecordsBuckets
 
 from cds.modules.deposit.api import CDSDeposit
 from cds.modules.deposit.links import deposit_links_factory
+from cds.modules.deposit.permissions import can_edit_deposit
+from cds.modules.deposit.views import to_links_js
 
 
 def test_deposit_link_factory_has_bucket(app, db, location):
@@ -62,6 +68,35 @@ def test_deposit_link_factory_has_bucket(app, db, location):
 
 
 def test_cds_deposit(location):
+    """Test CDS deposit creation."""
     deposit = CDSDeposit.create({})
     assert '_buckets' in deposit
     assert not deposit.is_published()
+
+
+def test_links_filter(location):
+    """Test Jinja to_links_js filter."""
+    assert to_links_js(None) == []
+    deposit = CDSDeposit.create({})
+    links = to_links_js(deposit.pid, deposit)
+    assert all([key in links for key in ['self', 'edit', 'publish', 'bucket',
+               'files', 'html', 'discard']])
+    self_url = links['self']
+    assert links['discard'] == self_url + '/actions/discard'
+    assert links['edit'] == self_url + '/actions/edit'
+    assert links['publish'] == self_url + '/actions/publish'
+    assert links['files'] == self_url + '/files'
+
+
+def test_permissions(location):
+    """Test deposit permissions."""
+    deposit = CDSDeposit.create({})
+    deposit.commit()
+    user = User(email='user@cds.cern', password='123456', active=True)
+    g.identity = Identity(user.id)
+    db.session.add(user)
+    db.session.commit()
+    login_user(user, force=True)
+    assert not can_edit_deposit(deposit)
+    deposit['_deposit']['owners'].append(user.id)
+    assert can_edit_deposit(deposit)
