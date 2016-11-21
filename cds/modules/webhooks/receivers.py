e3237cdbd07701d9be3fac0f2bb23191dab9a082
Leonardo Rossi <leonardo.r@cern.ch>
2016-11-21 09:49:29 +0100

    webhooks: status and info extraction
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

--- cds/modules/webhooks/receivers.py
+++ cds/modules/webhooks/receivers.py
@@ -21,15 +21,18 @@
 # In applying this license, CERN does not
 # waive the privileges and immunities granted to it by virtue of its status
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
-"""Webhook Receivers"""
+
+"""Webhook Receivers."""
 
 from __future__ import absolute_import
 
 from celery import chain, group
-from flask import url_for
 from invenio_db import db
+from celery import states
 from invenio_webhooks.models import Receiver
 from sqlalchemy.orm.attributes import flag_modified
+from celery.result import result_from_tuple
+import json
 
 from invenio_files_rest.models import (ObjectVersion, ObjectVersionTag,
                                        as_object_version)
@@ -38,15 +41,81 @@ from .tasks import (download_to_object_version, video_extract_frames,
                     video_metadata_extraction, video_transcode)
 
 
+def _compute_status(statuses):
+    for status_to_check in [states.FAILURE, states.STARTED, states.RETRY,
+                            states.PENDING]:
+        if any(status == status_to_check for status in statuses):
+            return status_to_check
+    return states.SUCCESS
+
+
+def _info_extractor(res, name, children=None):
+    """Return all tasks information."""
+    info = {'id': res.id}
+    if hasattr(res, 'status'):
+        info['status'] = res.status
+    if hasattr(res, 'info'):
+        info['info'] = str(res.info)
+    if children:
+        info['next'] = children
+    if hasattr(res, 'result'):
+        info['result'] = str(res.result)
+    info['name'] = name
+    return info
+
+
 class CeleryAsyncReceiver(Receiver):
     """TODO."""
 
-    def status(self, event):
-        """TODO."""
+    CELERY_STATES_TO_HTTP = {
+        states.PENDING: 202,
+        states.STARTED: 202,
+        states.RETRY: 202,
+        states.FAILURE: 500,
+        states.SUCCESS: 201,
+        states.REVOKED: 409,
+    }
+    """Mapping of Celery result states to HTTP codes."""
+
+    @classmethod
+    def _deserialize_result(cls, event):
+        """Deserialize celery result stored in event."""
+        result = result_from_tuple(event.response['_tasks']['result'])
+        parent = result_from_tuple(event.response['_tasks']['parent']) \
+            if 'parent' in event.response['_tasks'] else None
+        result.parent = parent
+        return result
+
+    @classmethod
+    def _serialize_result(cls, event, result):
+        """Run the task and save the task ids."""
+        with db.session.begin_nested():
+            event.response.update(
+                _tasks={
+                    'result': result.as_tuple(),
+                }
+            )
+            if result.parent:
+                event.response['_tasks']['parent'] = result.parent.as_tuple()
+            flag_modified(event, 'response')
+            flag_modified(event, 'response_headers')
 
     def delete(self, event):
         """TODO."""
-        pass
+        result = self._deserialize_result(event)
+        result.revoke()
+
+    def _make_status_response(self, status, info):
+        """Make a response."""
+        return (
+            CeleryAsyncReceiver.CELERY_STATES_TO_HTTP.get(status),
+            json.dumps(info)
+        )
+
+    def status(self, event):
+        """Get the status."""
+        return self._make_status_response(**self._status_and_info(
+            event=event))
 
 
 class Downloader(CeleryAsyncReceiver):
@@ -86,23 +155,33 @@ class Downloader(CeleryAsyncReceiver):
             db.session.expunge(event)
         db.session.commit()
 
-        task = download_to_object_version.s(event.payload['uri'],
-                                            str(object_version.version_id),
-                                            event_id=event_id,
-                                            **event.payload).apply_async()
+        task = download_to_object_version.s(
+            #event.payload['uri'],
+            object_version=str(object_version.version_id),
+            event_id=event_id,
+            **event.payload)
+
+        self._serialize_result(event=event, result=task.apply_async())
 
         with db.session.begin_nested():
             object_version = as_object_version(object_version.version_id)
-            event.response = dict(
-                _tasks=task.as_tuple(),
+            event.response.update(
                 links=dict(),
                 key=object_version.key,
                 version_id=str(object_version.version_id),
-                tags=object_version.get_tags(), )
+                tags=object_version.get_tags(),
+            )
             flag_modified(event, 'response')
             flag_modified(event, 'response_headers')
             db.session.add(event)
         db.session.commit()
+
+    def _status_and_info(self, event, fun=_info_extractor):
+        """Get status and info from the event."""
+        result = self._deserialize_result(event)
+        status = result.status
+        info = fun(result, 'download')
+        return {'status': status, 'info': info}
 
 
 class AVCWorkflow(CeleryAsyncReceiver):
@@ -149,9 +228,9 @@ class AVCWorkflow(CeleryAsyncReceiver):
             if 'version_id' in event.payload:
                 object_version = as_object_version(event.payload['version_id'])
                 first_step = video_metadata_extraction.si(
-                    object_version.file.uri,
-                    str(object_version.version_id),
-                    event.payload['deposit_id'])
+                    uri=object_version.file.uri,
+                    object_version=str(object_version.version_id),
+                    deposit_id=event.payload['deposit_id'])
             else:
                 object_version = ObjectVersion.create(
                     bucket=event.payload['bucket_id'],
@@ -160,43 +239,80 @@ class AVCWorkflow(CeleryAsyncReceiver):
                                         event.payload['uri'])
                 first_step = group(
                     download_to_object_version.si(
-                        event.payload['uri'],
-                        str(object_version.version_id),
+                        #  event.payload['uri'],
+                        object_version=str(object_version.version_id),
                         event_id=event_id,
                         **event.payload),
                     video_metadata_extraction.si(
-                        event.payload['uri'],
-                        str(object_version.version_id),
+                        #  event.payload['uri'],
+                        object_version=str(object_version.version_id),
                         event_id=event_id,
-                        **event.payload), )
+                        **event.payload),
+                )
 
             ObjectVersionTag.create(object_version, '_event_id', event_id)
 
+        mypayload = event.payload
         obj_id = str(object_version.version_id)
         obj_key = object_version.key
         obj_tags = object_version.get_tags()
         db.session.expunge(event)
         db.session.commit()
 
-        tasks = chain(
+        result = chain(
             first_step,
             group(
-                video_transcode.si(obj_id,
+                video_transcode.si(object_version=obj_id,
                                    event_id=event_id,
-                                   **event.payload),
-                video_extract_frames.si(str(object_version.version_id),
+                                   **mypayload),
+                video_extract_frames.si(object_version=str(obj_id),
                                         event_id=event_id,
-                                        **event.payload), ),
+                                        **mypayload), ),
         ).apply_async()
 
         with db.session.begin_nested():
-            event.response = dict(
-                _tasks=tasks.as_tuple(),
+            self._serialize_result(event=event, result=result)
+
+            event.response.update(
                 links=dict(),
                 key=object_version.key,
-                version_id=object_version.versrion_id,
-                tags=object_version.get_tags(), )
+                version_id=obj_id,
+                tags=obj_tags,
+            )
             flag_modified(event, 'response')
             flag_modified(event, 'response_headers')
             db.session.add(event)
         db.session.commit()
+
+    def _status_and_info(self, event, fun=_info_extractor):
+        """Get status and info from the event."""
+        result = self._deserialize_result(event)
+        if 'version_id' in event.payload:
+            status = _compute_status([
+                result.parent.status,
+                result.children[0].status,
+                result.children[1].status
+            ])
+            info = fun(result.parent, 'metadata-extractor', [
+                fun(result.children[0], 'transcode'),
+                fun(result.children[1], 'extract-frames')
+            ])
+        else:
+            status = _compute_status([
+                result.parent.children[0].status,
+                result.parent.children[1].status,
+                result.children[0].status,
+                result.children[1].status,
+            ])
+            info = [
+                [
+                    fun(result.parent.children[0], 'download'),
+                    fun(
+                        result.parent.children[1], 'metadata-extractor'),
+                ],
+                [
+                    fun(result.children[0], 'transcode'),
+                    fun(result.children[1], 'extract-frames'),
+                ]
+            ]
+        return {'status': status, 'info': info}
--- tests/unit/test_webhook_receivers.py
+++ tests/unit/test_webhook_receivers.py
@@ -27,7 +27,6 @@
 from __future__ import absolute_import, print_function
 
 import json
-from time import sleep
 
 import mock
 from flask import url_for
@@ -35,6 +34,17 @@ from flask import url_for
 from invenio_files_rest.models import ObjectVersion
 from invenio_pidstore.models import PersistentIdentifier
 from invenio_records import Record
+import pytest
+
+from celery import states, chain, group
+from celery.result import AsyncResult
+from cds.modules.webhooks.receivers import CeleryAsyncReceiver
+from invenio_webhooks import current_webhooks
+from cds.modules.webhooks.receivers import _compute_status
+from cds.modules.webhooks.receivers import _info_extractor
+from invenio_webhooks.models import Event
+
+from helpers import failing_task, success_task, simple_add
 
 
 def test_download_receiver(api_app, db, bucket, depid, access_token,
@@ -48,14 +58,26 @@ def test_download_receiver(api_app, db, bucket, depid, access_token,
             access_token=access_token)
 
     with mock.patch('requests.get') as mock_request, \
+            mock.patch('invenio_sse.ext._SSEState.publish') as mock_sse, \
             api_app.test_client() as client:
-        mock_request.return_value = type('Response', (object, ),
-                                         {'content': b'\x00' * 1024})
+
+        sse_channel = 'mychannel'
+        mock_sse.return_value = None
+
+        file_size = 1024
+        mock_request.return_value = type(
+            'Response', (object, ), {
+                'content': b'\x00' * file_size,
+                'headers': {'Content-Length': file_size}
+            })
+
         payload = dict(
             uri='http://example.com/test.pdf',
             bucket_id=str(bucket.id),
             deposit_id=depid,
-            key='test.pdf')
+            key='test.pdf',
+            sse_channel=sse_channel
+        )
         resp = client.post(url, headers=json_headers, data=json.dumps(payload))
 
         assert resp.status_code == 202
@@ -74,9 +96,48 @@ def test_download_receiver(api_app, db, bucket, depid, access_token,
         assert obj.key == data['key']
         assert str(obj.version_id) == data['version_id']
         assert obj.file
-        assert obj.file.size == 1024
+        assert obj.file.size == file_size
 
-        # TODO get status after POST
+        # check sse is called
+        assert mock_sse.called
+
+        def set_data(state, message, size, total, percentage):
+            return {
+                'state': state,
+                'meta': {
+                    'message': message,
+                    'payload': {
+                        'event_id': str(tags['_event_id']),
+                        'key': u'test.pdf',
+                        'tags': {
+                            u'uri_origin': u'http://example.com/test.pdf',
+                            u'_event_id': str(tags['_event_id']),
+                        },
+                        'deposit_id': depid,
+                        'percentage': percentage,
+                        'version_id': str(obj.version_id),
+                        'size': size,
+                        'total': total,
+                    }
+                }
+            }
+        assert mock_sse.call_count == 3
+        mock_sse.assert_any_call(
+            data=set_data(
+                states.STARTED,
+                'Downloading {} of {}'.format(file_size, file_size),
+                file_size, file_size, 100
+            ),
+            channel=u'mychannel',
+            type_='file_download'
+        )
+        mock_sse.assert_any_call(
+            data=set_data(
+                states.SUCCESS, str(obj.version_id), file_size, file_size, 100
+            ),
+            channel=u'mychannel',
+            type_='file_download'
+        )
 
 
 def test_avc_workflow_receiver(api_app, db, bucket, depid, access_token,
@@ -111,7 +172,9 @@ def test_avc_workflow_receiver(api_app, db, bucket, depid, access_token,
         assert 'version_id' in data
         assert 'links' in data  # TODO decide with links are needed
 
-        assert ObjectVersion.query.count() == 2
+        # 1 original + 1 slave + 90 frames == 92
+        assert ObjectVersion.query.count() == 92
+
         # Master file
         master = ObjectVersion.get(bucket_id, master_key)
         tags = master.get_tags()
@@ -137,10 +200,163 @@ def test_avc_workflow_receiver(api_app, db, bucket, depid, access_token,
         # Slave file
         slave = ObjectVersion.get(bucket_id, slave_key)
         tags = slave.get_tags()
-        assert 'job_id' in tags
-        assert tags['job_id'] == '1234'
+        assert '_sorenson_job_id' in tags
+        assert tags['_sorenson_job_id'] == '1234'
         assert 'master' in tags
         assert tags['master'] == str(master.version_id)
         assert slave.key == 'test-Youtube 480p.mp4'
         assert master.file
         assert master.file.size == video_size
+
+
+@pytest.mark.parametrize(
+    'receiver_id,workflow,status,http_status, payload,result', [
+        ('failing-task', failing_task, states.FAILURE, 500, {}, None),
+        ('success-task', success_task, states.SUCCESS, 201, {}, None),
+        ('add-task', simple_add, states.SUCCESS, 202, {'x': 40, 'y': 2}, 42)
+    ]
+)
+def test_async_receiver_status_fail(api_app, access_token, u_email,
+                                    json_headers, receiver_id, workflow,
+                                    status, http_status, payload, result):
+    """Test AVC workflow test-case."""
+    ctx = dict()
+
+    class TestReceiver(CeleryAsyncReceiver):
+
+        def run(self, event):
+            assert payload == event.payload
+            ctx['myresult'] = workflow.s(**event.payload).apply_async()
+            self._serialize_result(event, ctx['myresult'])
+
+        def _status_and_info(self, event):
+            result = self._deserialize_result(event)
+            status = result.status
+            info = _info_extractor(result, receiver_id)
+            return dict(status=status, info=info)
+
+    current_webhooks.register(receiver_id, TestReceiver)
+
+    with api_app.test_request_context():
+        url = url_for('invenio_webhooks.event_list',
+                      receiver_id=receiver_id,
+                      access_token=access_token)
+    with api_app.test_client() as client:
+        # run the task
+        resp = client.post(url, headers=json_headers, data=json.dumps(payload))
+        assert resp.status_code == http_status
+        data = json.loads(resp.headers['X-Hub-Info'])
+        assert data['name'] == receiver_id
+        extra_info = json.loads(resp.headers['X-Hub-Info'])
+        assert extra_info['id'] == ctx['myresult'].id
+        assert ctx['myresult'].result == result
+
+    with api_app.test_request_context():
+        event_id = resp.headers['X-Hub-Delivery']
+        url = url_for('invenio_webhooks.event_item',
+                      receiver_id=receiver_id, event_id=event_id,
+                      access_token=access_token)
+    with api_app.test_client() as client:
+        # check status
+        resp = client.get(url, headers=json_headers)
+        assert resp.status_code == http_status
+        data = json.loads(resp.headers['X-Hub-Info'])
+        #  assert data['status'] == status
+        assert data['name'] == receiver_id
+        extra_info = json.loads(resp.headers['X-Hub-Info'])
+        assert extra_info['id'] == ctx['myresult'].id
+
+
+def test_compute_status():
+    """Test compute status."""
+    assert states.FAILURE == _compute_status([
+        states.STARTED, states.RETRY, states.PENDING, states.FAILURE,
+        states.SUCCESS])
+    assert states.STARTED == _compute_status([
+        states.RETRY, states.PENDING, states.STARTED, states.SUCCESS])
+    assert states.RETRY == _compute_status([
+        states.RETRY, states.PENDING, states.SUCCESS, states.SUCCESS])
+    assert states.PENDING == _compute_status([
+        states.PENDING, states.PENDING, states.SUCCESS, states.SUCCESS])
+    assert states.SUCCESS == _compute_status([states.SUCCESS, states.SUCCESS])
+
+
+def test_info_extractor():
+    """Test info extractor."""
+    result = simple_add.s(1, 2).apply_async()
+
+    info = _info_extractor(result, 'mytask')
+    assert info['status'] == states.SUCCESS
+    assert info['info'] == '3'
+    assert 'id' in info
+    assert info['name'] == 'mytask'
+    assert info['result'] == '3'
+
+    result = chain(simple_add.s(1, 2), simple_add.s(3)).apply_async()
+
+    # check first task
+    info = _info_extractor(result.parent, 'mytask')
+    assert info['status'] == states.SUCCESS
+    assert info['info'] == '3'
+    assert 'id' in info
+    assert info['name'] == 'mytask'
+    assert info['result'] == '3'
+    # check second task
+    info = _info_extractor(result, 'mytask2')
+    assert info['status'] == states.SUCCESS
+    assert info['info'] == '6'
+    assert 'id' in info
+    assert info['name'] == 'mytask2'
+    assert info['result'] == '6'
+
+    result = chain(
+        simple_add.s(1, 2),
+        group(simple_add.s(3), simple_add.s(4), failing_task.s())
+    ).apply_async()
+
+    info = _info_extractor(result.parent, 'mytask')
+    assert info['status'] == states.SUCCESS
+    assert info['info'] == '3'
+    assert 'id' in info
+    assert info['name'] == 'mytask'
+    assert info['result'] == '3'
+
+    info = _info_extractor(result.children[0], 'mytask2')
+    assert info['status'] == states.SUCCESS
+    assert info['info'] == '6'
+    assert 'id' in info
+    assert info['name'] == 'mytask2'
+    assert info['result'] == '6'
+
+    info = _info_extractor(result.children[1], 'mytask3')
+    assert info['status'] == states.SUCCESS
+    assert info['info'] == '7'
+    assert 'id' in info
+    assert info['name'] == 'mytask3'
+    assert info['result'] == '7'
+
+    fail = AsyncResult(result.children[2].id)
+    info = _info_extractor(fail, 'mytask4')
+    assert info['status'] == states.FAILURE
+    assert 'id' in info
+    assert info['name'] == 'mytask4'
+
+
+def test_serializer():
+    """Test result serializer on event."""
+    event = Event()
+    event.response = {}
+
+    result = chain(
+        simple_add.s(1, 2),
+        group(simple_add.s(3), simple_add.s(4), failing_task.s())
+    ).apply_async()
+
+    CeleryAsyncReceiver._serialize_result(event=event, result=result)
+    deserialized_result = CeleryAsyncReceiver._deserialize_result(event=event)
+
+    assert deserialized_result.id == result.id
+    assert deserialized_result.parent.id == result.parent.id
+    assert deserialized_result.children[0].id == result.children[0].id
+    assert deserialized_result.children[1].id == result.children[1].id
+    assert deserialized_result.children[2].id == result.children[2].id
