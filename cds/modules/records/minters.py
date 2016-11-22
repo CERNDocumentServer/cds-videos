57a845defc8ce5af4ec39118262ea210e46e878b
Sourabh Lal <sourabh.lal@cern.ch>
2016-11-22 13:53:17 +0100

    records: addition of DOI minter
        * Adds the DOI minter. (closes #182)
    
        Signed-off-by: Sourabh Lal <sourabhlal@gmail.com>

--- cds/modules/records/minters.py
+++ cds/modules/records/minters.py
@@ -24,10 +24,14 @@
 
 """Persistent identifier minters."""
 
+
+
 from __future__ import absolute_import, print_function
 
 from .providers import CDSRecordIdProvider
-
+from invenio_pidstore.models import PersistentIdentifier, PIDStatus
+import idutils
+from flask import current_app
 
 def recid_minter(record_uuid, data):
     """Mint record identifiers."""
@@ -35,4 +39,36 @@ def recid_minter(record_uuid, data):
     provider = CDSRecordIdProvider.create(
         object_type='rec', object_uuid=record_uuid)
     data['recid'] = int(provider.pid.pid_value)
+    
+    cds_doi_minter(record_uuid, data)
+
     return provider.pid
+
+def cds_doi_generator(recid, prefix=None):
+    """Generate a DOI."""
+    return '{prefix}/cds.{recid}'.format(
+        prefix=prefix or current_app.config['PIDSTORE_DATACITE_DOI_PREFIX'],
+        recid=recid
+)
+
+def cds_doi_minter(record_uuid, data):
+    """Mint DOI."""
+    doi = data.get('doi')
+    assert 'recid' in data
+
+    # Create a DOI if no DOI was found.
+    if not doi:
+        doi = cds_doi_generator(data['recid'])
+        import ipdb; ipdb.set_trace()
+        data['doi'] = doi
+
+    # Make sure it's a proper DOI
+    assert idutils.is_doi(doi)
+
+    return PersistentIdentifier.create(
+    	'doi',
+    	doi,
+    	object_type='rec',
+    	object_uuid=record_uuid,
+    	status=PIDStatus.RESERVED
+    )
--- setup.py
+++ setup.py
@@ -186,6 +186,15 @@ setup(
         ],
         'invenio_pidstore.minters': [
             'cds_recid = cds.modules.records.minters:recid_minter',
+            'cds_depid = cds.modules.deposit.minters:cds_deposit_minter',
+        ],
+        'invenio_pidstore.fetchers': [
+            'cds_record_fetcher '
+            '= cds.modules.records.fetchers:cds_record_fetcher',
+            'cds_deposit_fetcher '
+            '= cds.modules.deposit.fetchers:cds_deposit_fetcher',
+            'cds_doi_fetcher '
+            '= cds.modules.records.fetchers:cds_doi_fetcher',
         ],
         'invenio_i18n.translations': [
             'messages = cds',
