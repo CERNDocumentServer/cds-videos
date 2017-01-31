eeb2c6217f8c63fc604e45898ca6a28de7fafdf2
Harris Tzovanakis <me@drjova.com>
2017-01-31 10:28:43 +0100

    deposit: addition of REVOKED state
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- cds/modules/deposit/static/js/cds_deposit/avc/avc.module.js
+++ cds/modules/deposit/static/js/cds_deposit/avc/avc.module.js
@@ -33,6 +33,7 @@ function cdsDepositsConfig(
     STARTED: 'DEPOSIT_STATE/STARTED',
     FAILURE: 'DEPOSIT_STATE/FAILURE',
     SUCCESS: 'DEPOSIT_STATE/SUCCESS',
+    REVOKED: 'DEPOSIT_STATE/REVOKED',
   });
 
   // Initialize url builder
@@ -85,10 +86,9 @@ angular
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-        scope.skata = 'HELLO';
         var topClass = attrs.setClassWhenAtTop,
-          // get CSS class from directive's attribute value
-          offsetTop = element.offset().top;
+        // get CSS class from directive's attribute value
+        offsetTop = element.offset().top;
         // get element's top relative to the document
         $win.on('scroll', function(e) {
           if ($win.scrollTop() >= offsetTop) {
