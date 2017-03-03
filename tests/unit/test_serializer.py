e57b7efbcfdaa140d5b464e1815f0396e81580cb
Orestis Melkonian <melkon.or@gmail.com>
2017-03-03 15:33:27 +0100

    records: drupal serialization fix
     * Fixes issue with incorrect extraction of a video's creation date from
       its technical video metadata, rather than the record's
       `publication_date` field. (closes #500)
    
    Signed-off-by: Orestis Melkonian <melkon.or@gmail.com>

--- tests/unit/test_serializer.py
+++ tests/unit/test_serializer.py
@@ -28,6 +28,8 @@ from __future__ import absolute_import, print_function
 
 import xml.etree.ElementTree as ET
 
+from cds.modules.deposit.api import Video
+from cds.modules.records.serializers.drupal import VideoDrupal
 from cds.modules.records.serializers.smil import Smil
 from cds.modules.records.serializers.vtt import VTT
 
@@ -63,3 +65,49 @@ def test_vtt_serializer(video_record_metadata):
             assert data[i]['end_time'] == end_expected
         else:
             assert data[i]['end_time'] == data[i + 1]['start_time']
+
+
+def test_drupal_serializer(video_record_metadata, deposit_metadata):
+    """Test drupal serializer."""
+    duration = '00:01:00.140'
+    report_number = 'RN-01'
+    video_record_metadata.update(deposit_metadata)
+    video_record_metadata.update({
+        'report_number': {'report_number': report_number},
+        '$schema': Video.get_record_schema(),
+        'duration': duration,
+    })
+    expected = dict(
+        caption_en='in tempor reprehenderit enim eiusmod',
+        caption_fr='france caption',
+        copyright_date='2017',
+        copyright_holder='CERN',
+        creation_date='2017-03-02',
+        directors='paperone, pluto',
+        entry_date='2016-12-03',
+        id=report_number,
+        keywords='keyword1, keyword2',
+        license_body='GPLv2',
+        license_url='http://license.cern.ch',
+        producer='nonna papera, zio paperino',
+        record_id=1,
+        title_en='My english title',
+        title_fr='My french title',
+        type='video',
+        video_length=duration,
+    )
+
+    # Proper publication date
+    serializer = VideoDrupal(video_record_metadata)
+    data = serializer.format()['entries'][0]['entry']
+    data = {k: data[k] for k in data if k in expected}
+    assert data == expected
+
+    # Empty publication date
+    del video_record_metadata['publication_date']
+    expected['creation_date'] = ''
+
+    serializer = VideoDrupal(video_record_metadata)
+    data = serializer.format()['entries'][0]['entry']
+    data = {k: data[k] for k in data if k in expected}
+    assert data == expected
