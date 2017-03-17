d19c9a1a2370413ad696164e5e0580fb7832effc
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2017-03-17 11:42:50 +0100

    deposit: full autocompleted contributor info
    * Send the full info of autocompleted contributors as values instead of
      only the name.
    
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- cds/modules/deposit/bundles.py
+++ cds/modules/deposit/bundles.py
@@ -74,6 +74,7 @@ js_jquery = NpmBundle(
 
 js_deposit_common = Bundle(
     'js/cds_deposit/avc/avc.module.js',
+    'js/cds_deposit/avc/filters/mergeObjects.js',
     'js/cds_deposit/avc/filters/orderTasks.js',
     'js/cds_deposit/avc/filters/overallState.js',
     'js/cds_deposit/avc/filters/progressClass.js',
