190b7bcfdc283d10f8afa86f78dfafb93ddc0604
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2017-03-17 11:42:50 +0100

    deposit: full autocompleted contributor info
    * Send the full info of autocompleted contributors as values instead of
      only the name.
    
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

---
+++ cds/modules/deposit/static/js/cds_deposit/avc/filters/mergeObjects.js
@@ -0,0 +1,9 @@
+function mergeObjects() {
+  // Filter to allow merging of two objects inside angular expressions
+  return function(dst, src) {
+    return angular.merge(dst, src);
+  }
+}
+
+angular.module('cdsDeposit.filters')
+  .filter('mergeObjects', mergeObjects);
