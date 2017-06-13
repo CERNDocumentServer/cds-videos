caa0114ecb526ca755fbd9959350493d63018a18
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2017-06-13 15:48:39 +0200

    deposit: record restriction improvements
    * Adds email suggestions for record restrictions.
    
    * Fixes record restrictions disappearing when refreshing a deposit.
      (closes #905)
    
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- cds/modules/deposit/static/js/cds_deposit/avc/components/cdsDeposits.js
+++ cds/modules/deposit/static/js/cds_deposit/avc/components/cdsDeposits.js
@@ -89,6 +89,11 @@ function cdsDepositsCtrl(
         }
       });
     }
+
+    that.categoriesPromise = $http.get(urlBuilder.categories()).then(
+      function(data) {
+        return data.data.hits.hits;
+      });
   };
 
   this.addMaster = function(deposit, files) {
