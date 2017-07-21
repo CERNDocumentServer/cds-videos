d8c1b4482f25d01823c6d1f265bfe30cbd4a6ac0
Harris Tzovanakis <me@drjova.com>
2017-07-21 18:03:09 +0200

    previewer: addition of THEOPlayer v2
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- cds/modules/previewer/api.py
+++ cds/modules/previewer/api.py
@@ -74,6 +74,25 @@ class CDSPreviewRecordFile(PreviewFile):
                 filename='frame-1.jpg')
 
     @property
+    def record_uri(self):
+        """Get the record uri."""
+        return url_for(
+            'invenio_records_rest.recid_item',
+            pid_value=self.pid.pid_value,
+            _external=True
+        )
+
+    @property
+    def embed_uri(self):
+        """Get the embed uri."""
+        return url_for(
+            'invenio_records_ui.recid_preview',
+            pid_value=self.pid.pid_value,
+            filename=self.file.key,
+            _external=True
+        )
+
+    @property
     def thumbnails_uri(self):
         """Get video's thumbnails' link."""
         return url_for(
