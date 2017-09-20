b854f8d7d0fa11fbc4fb353055217472d0b24919
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2017-09-20 09:59:53 +0200

    serializers: add missing fields after migration

--- cds/modules/records/serializers/schemas/video.py
+++ cds/modules/records/serializers/schemas/video.py
@@ -20,16 +20,15 @@
 
 from __future__ import absolute_import
 
-from marshmallow import fields, post_load, Schema
 from invenio_jsonschemas import current_jsonschemas
+from marshmallow import Schema, fields, post_load
 
 from ....deposit.api import Video
 from ..fields.datetime import DateString
-from .common import \
-    AccessSchema, BucketSchema, ContributorSchema, \
-    DepositSchema, KeywordsSchema, LicenseSchema, \
-    OaiSchema, RelatedLinksSchema, StrictKeysSchema, \
-    TitleSchema, TranslationsSchema
+from .common import (AccessSchema, BucketSchema, ContributorSchema,
+                     DepositSchema, KeywordsSchema, LicenseSchema, OaiSchema,
+                     RelatedLinksSchema, StrictKeysSchema, TitleSchema,
+                     TranslationsSchema)
 from .doi import DOI
 
 
@@ -83,37 +82,68 @@ class AcceleratorExperimentSchema(StrictKeysSchema):
     facility = fields.Str()
 
 
+class PhysicalMediumSchema(StrictKeysSchema):
+    """Field physical medium."""
+
+    arrangement = fields.Str()
+    bar_code = fields.Str()
+    camera = fields.Str()
+    copy_number = fields.Str()
+    internal_note = fields.Str()
+    location = fields.Str()
+    medium_standard = fields.Str()
+    note = fields.Str()
+    sequence_number = fields.Str()
+    shelf = fields.Str()
+
+
+class ExternalSystemIdentifiersField(StrictKeysSchema):
+    """Field physical medium."""
+
+    value = fields.Str()
+    schema = fields.Str()
+
+
 class VideoSchema(StrictKeysSchema):
     """Video schema."""
 
-    _deposit = fields.Nested(VideoDepositSchema, required=True)
-    _cds = fields.Nested(_CDSSSchema, required=True)
-    title = fields.Nested(TitleSchema, required=True)
-    description = fields.Str(required=True)
-    date = DateString(required=True)
-    category = fields.Str()
-    type = fields.Str()
-
-    recid = fields.Number()
     _access = fields.Nested(AccessSchema)
     _buckets = fields.Nested(BucketSchema)
+    _cds = fields.Nested(_CDSSSchema, required=True)
+    _deposit = fields.Nested(VideoDepositSchema, required=True)
     _oai = fields.Nested(OaiSchema)
     _project_id = fields.Str()
+    accelerator_experiment = fields.Nested(AcceleratorExperimentSchema)
+    agency_code = fields.Str()
+    category = fields.Str()
     contributors = fields.Nested(ContributorSchema, many=True, required=True)
     copyright = fields.Nested(CopyrightSchema)
+    date = DateString(required=True)
+    description = fields.Str(required=True)
     doi = DOI()
-    vr = fields.Boolean()
     duration = fields.Str()
+    external_system_identifiers = fields.Nested(
+        ExternalSystemIdentifiersField, many=True)
     featured = fields.Boolean()
+    internal_note = fields.Str()
     keywords = fields.Nested(KeywordsSchema, many=True)
     language = fields.Str()
     license = fields.Nested(LicenseSchema, many=True)
-    related_links = fields.Nested(RelatedLinksSchema, many=True)
-    schema = fields.Str(attribute="$schema", dump_to='$schema')
-    translations = fields.Nested(TranslationsSchema, many=True)
-    report_number = fields.List(fields.Str, many=True)
+    note = fields.Str()
     publication_date = fields.Str()
-    accelerator_experiment = fields.Nested(AcceleratorExperimentSchema)
+    recid = fields.Number()
+    related_links = fields.Nested(RelatedLinksSchema, many=True)
+    report_number = fields.List(fields.Str, many=True)
+    schema = fields.Str(attribute="$schema", dump_to='$schema')
+    title = fields.Nested(TitleSchema, required=True)
+    translations = fields.Nested(TranslationsSchema, many=True)
+    type = fields.Str()
+    vr = fields.Boolean()
+
+    # Preservation fields
+    location = fields.Str()
+    original_source = fields.Str()
+    physical_medium = fields.Nested(PhysicalMediumSchema, many=True)
 
     @post_load(pass_many=False)
     def post_load(self, data):
