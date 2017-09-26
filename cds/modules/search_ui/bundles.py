8aa04c19f48d9d6e3b4a3f92ff52d93c9901b907
Nicola Tarocco <nicola.tarocco@cern.ch>
2017-09-26 17:30:58 +0200

    global: added reset link on year facet

--- cds/modules/search_ui/bundles.py
+++ cds/modules/search_ui/bundles.py
@@ -38,7 +38,7 @@ js = NpmBundle(
     npm={
         'angular-loading-bar': '~0.9.0',
         'd3': '^3.5.17',
-        'invenio-search-js': '~1.2.1',
+        'invenio-search-js': '~1.2.2',
     },
 )
 
