5a2ecb9cdd829f7b2a8b355f060daa880d7b8c24
ioanungurean <inungurean@gmail.com>
2018-05-14 18:53:57 +0200

    ui: fix broken get suggestions on search
    Closes #1592

--- cds/modules/theme/static/js/main.js
+++ cds/modules/theme/static/js/main.js
@@ -22,111 +22,39 @@
 */
 
 // Bootstrap modules
-angular.element(document).ready(function() {
+angular.element(document).ready(function () {
   angular.bootstrap(
-    document.getElementById("invenio-search"), ['cds', 'angular-loading-bar', 'ngDialog', 'invenioSearch']
+    document.getElementById('invenio-search'), ['cds', 'angular-loading-bar', 'ngDialog', 'invenioSearch']
   );
   angular.bootstrap(
-    document.getElementById("cds-featured-video"), [ 'cds', 'invenioSearch']
+    document.getElementById('cds-featured-video'), ['cds', 'invenioSearch']
   );
   angular.bootstrap(
-    document.getElementById("cds-recent-videos"), [ 'cds', 'invenioSearch']
+    document.getElementById('cds-recent-videos'), ['cds', 'invenioSearch']
   );
 });
 
-var app = angular.module('cdsSuggest', ['ngSanitize', 'MassAutoComplete', 'LocalStorageModule']);
-app.controller('mainCtrl', function ($scope, $sce, $q, $http, localStorageService) {
+function mainCtrl($scope, $sce, $q, $http, localStorageService, searchSuggestions) {
   $scope.dirty = {};
-  var url = '/api/records/';
-  function onAttach() {
-    $scope.focused = true;
+
+  $scope.updateHistory = function () {
+    searchSuggestions.onSelect({ label: $scope.dirty.value, value: $scope.dirty.value }, true);
   }
-  function suggest_state_remote(term) {
-    var deferred = $q.defer();
-    $http({
-      method: 'GET',
-      url: url,
-      params: {
-        q: term
-      }
-    }).then(function(response) {
-      var results = [];
-      if (response.data.hits.hits) {
-        angular.forEach(response.data.hits.hits, function(value, index) {
-          results.push({
-            label: value.metadata.title.title,
-            value: value.metadata.title.title
-          })
-        });
-      }
-      results = results.concat(localStorageService.get('cds.search.history') || []);
-      deferred.resolve(results);
-    });
-    return deferred.promise;
-  }
-  function onSelect(selected, preventFormSubmit) {
-    if (selected && !_.isEmpty(selected.value)){
-      try {
-        var searches = localStorageService.get('cds.search.history') || [];
-        var exists = _.find(searches, {value: selected.value});
-        if (exists === undefined) {
-          if (searches.length > 4){
-            searches.pop();
-          }
-          selected.label = '<i class="fa fa-history text-primary pr-5"></i> ' + selected.label;
-          searches.push(selected);
-          localStorageService.set('cds.search.history', searches);
-        }
-        // submit form to trigger search
-        if (!preventFormSubmit) {
-          // it is needed because when you selected an option by clicking not
-          // hiting enter angular was throwing na `Error: [$rootScope:inprog]
-          // $apply already in progress` trying to use $apply when angular is
-          // running a $digest cycle. The error probably is comming from
-          // massautocomplete.js that we use for autocompletion
-          setTimeout(function() {
-            $("[name='cdsSearchFormSuggest']").submit();
-          });
-        }
-      } catch(error) {
-        // Error no worries..
-      }
-    }
-  }
-  $scope.updateHistory = function() {
-    onSelect({label: $scope.dirty.value, value: $scope.dirty.value}, true);
-  }
+
   $scope.autocomplete_options = {
-    suggest: suggest_state_remote,
-    on_attach: onAttach,
-    on_select: onSelect,
+    suggest: searchSuggestions.suggestStateRemote,
+    on_select: searchSuggestions.onSelect,
+    on_attach: function () { $scope.focused = true; },
   };
-});
+};
 
-// Make sure navigation is on focus
-$(document).ready(function() {
-  // load and show any announcement message
-  toggleAnnouncement();
+angular.module('cdsSuggest', ['MassAutoComplete', 'cdsSharedServices'])
+  .controller('mainCtrl', mainCtrl);
 
-  $('#cds-navbar-form-input').focus(function() {
-    $(".cds-navbar-form").addClass('cds-active-search');
-  })
-  .blur(function() {
-    $(".cds-navbar-form").removeClass('cds-active-search');
-  });
-
-  // Focus when pressing ``l``
-  Mousetrap.bind('l', function() {
-    $('#cds-navbar-form-input').focus();
-    setTimeout(function() {
-      $('#cds-navbar-form-input').val('');
-    },0);
-  });
-
-  Mousetrap.bind('c d s g r e a t', function() {
-    // Start rainbow show
-    rainbowShow();
-  });
+/**
+ * Additional fun features
+ */
+$(document).ready(function () {
   function rainbowShow() {
     var _c = '.unicorn-rainbow';
     if ($(_c).length == 0) {
@@ -137,10 +65,10 @@ $(document).ready(function() {
         })
       );
     }
-    $(_c).css({left:0}).show();
+    $(_c).css({ left: 0 }).show();
     $(_c).animate({
       left: $(window).width(),
-    }, 1000, function() {
+    }, 1000, function () {
       $(_c).hide();
     });
   }
@@ -153,11 +81,32 @@ $(document).ready(function() {
           .removeClass('hidden')
           .html(result.message);
 
-        // hack to fix wrong margin on deposit view
+        // Hack to fix wrong margin on deposit view
         $('#cds-deposit').addClass('fix-margin-top');
       } else {
         $('#announcement').addClass('hidden');
       }
     });
   }
+
+  // Load and show any announcement message
+  toggleAnnouncement();
+
+  $('#cds-navbar-form-input').focus(function () {
+    $('.cds-navbar-form').addClass('cds-active-search');
+  }).blur(function () {
+    $('.cds-navbar-form').removeClass('cds-active-search');
+  });
+
+  // Focus when pressing ``l``
+  Mousetrap.bind('l', function () {
+    $('#cds-navbar-form-input').focus();
+    setTimeout(function () {
+      $('#cds-navbar-form-input').val('');
+    }, 0);
+  });
+
+  Mousetrap.bind('c d s g r e a t', function () {
+    rainbowShow();
+  });
 });
