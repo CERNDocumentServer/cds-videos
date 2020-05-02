cfc87bb789a893aa9b227fa09be5ebd002626721
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2020-05-02 17:47:53 -0400

    flows: new module
    * Internal port of Invenio-Flows to avoid package compatibility, i.e.
      Celery 3.x vs 4.x

---
+++ cds/modules/flows/models.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Document Server.
+# Copyright (C) 2020 CERN.
+#
+# CERN Document Server is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# CERN Document Server is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CERN Document Server; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Flow and task models."""
+
+import logging
+import uuid
+from enum import Enum, unique
+
+from invenio_db import db
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_utils.models import Timestamp
+from sqlalchemy_utils.types import JSONType, UUIDType
+
+logger = logging.getLogger('invenio-flow')
+
+
+def as_task(value):
+    """Get task object from task id or task object.
+
+    :param value: A :class:`invenio_flow.models.Task` or a Task Id.
+    :returns:  A :class:`invenio_flow.models.Task` instance.
+    """
+    return value if isinstance(value, Task) else Task.get(value)
+
+
+@unique
+class Status(Enum):
+    """Constants for possible task status."""
+
+    PENDING = 'PENDING'
+    SUCCESS = 'SUCCESS'
+    FAILURE = 'FAILURE'
+    CANCELED = 'CANCELED'
+
+    @classmethod
+    def compute_status(cls, statuses):
+        """Compute the general status from a list."""
+        if not statuses:
+            return cls.PENDING
+
+        if all(s == cls.SUCCESS for s in statuses):
+            return cls.SUCCESS
+
+        for status in (cls.PENDING, cls.FAILURE, cls.CANCELED):
+            if any(s == status for s in statuses):
+                return status
+
+        return cls.PENDING
+
+    def __str__(self):
+        """Return its value."""
+        return self.value
+
+
+class Flow(db.Model, Timestamp):
+    """Flow database model."""
+
+    __tablename__ = 'flows_flow'
+
+    id = db.Column(UUIDType, primary_key=True, default=uuid.uuid4,)
+    """Flow identifier."""
+
+    name = db.Column(
+        db.String, nullable=False
+    )  # TODO: Most likely I don't need this one
+    """Flow name."""
+
+    payload = db.Column(
+        db.JSON()
+        .with_variant(postgresql.JSONB(none_as_null=True), 'postgresql',)
+        .with_variant(JSONType(), 'sqlite',)
+        .with_variant(JSONType(), 'mysql',),
+        default=lambda: dict(),
+        nullable=True,
+    )
+    """Flow payload in JSON format, typically args and kwagrs."""
+
+    previous_id = db.Column(
+        UUIDType,
+        db.ForeignKey('flows_flow.id', onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=True,
+    )
+    """Task flow instance."""
+
+    @hybrid_property
+    def status(self):
+        """Overall flow status computed from tasks statuses."""
+        return Status.compute_status([t.status for t in self.tasks])
+
+    def __repr__(self):
+        """Flow representation."""
+        return '<Workflow {name} {status}: {payload}>'.format(self)
+
+    @classmethod
+    def get(cls, id_):
+        """Get a flow object from the DB."""
+        return cls.query.get(id_)
+
+    def to_dict(self):
+        """Flow dictionary representation."""
+        return {
+            'id': str(self.id),
+            'created': self.created.isoformat(),
+            'updated': self.updated.isoformat(),
+            'name': self.name,
+            'payload': self.payload,
+            'status': str(self.status),
+            'previous': str(self.previous_id),
+        }
+
+
+class Task(db.Model, Timestamp):
+    """Task database model."""
+
+    __tablename__ = 'flows_task'
+
+    id = db.Column(UUIDType, primary_key=True, default=uuid.uuid4,)
+    """Task identifier."""
+
+    previous = db.Column(
+        db.JSON()
+        .with_variant(postgresql.JSONB(none_as_null=True), 'postgresql',)
+        .with_variant(JSONType(), 'sqlite',)
+        .with_variant(JSONType(), 'mysql',),
+        default=lambda: list(),
+        nullable=True,
+    )
+    """List of tasks that need to run before this one, if any.
+
+    This is mainly used by visuals to create the flow diagram.
+    """
+
+    flow_id = db.Column(
+        UUIDType,
+        db.ForeignKey(Flow.id, onupdate="CASCADE", ondelete="CASCADE"),
+        nullable=False,
+    )
+    """Task flow instance."""
+
+    flow = db.relationship(Flow, backref='tasks')
+    """Relationship to the Flow."""
+
+    name = db.Column(db.String, nullable=False)
+    """Task name."""
+
+    payload = db.Column(
+        db.JSON()
+        .with_variant(postgresql.JSONB(none_as_null=True), 'postgresql',)
+        .with_variant(JSONType(), 'sqlite',)
+        .with_variant(JSONType(), 'mysql',),
+        default=lambda: dict(),
+        nullable=True,
+    )
+    """Flow payload in JSON format, typically args and kwagrs."""
+
+    status = db.Column(
+        db.Enum(Status), nullable=False, default=Status.PENDING,
+    )
+    """Status of the task, i.e. pending, success, failure."""
+
+    message = db.Column(db.String, nullable=False, default='')
+    """Task status message."""
+
+    @classmethod
+    def create(cls, name, flow_id, id_=None, payload=None, previous=None):
+        """Create a new Task."""
+        try:
+            with db.session.begin_nested():
+                obj = cls(
+                    id=id_ or uuid.uuid4(),
+                    flow_id=flow_id,
+                    name=name,
+                    payload=payload or {},
+                    previous=previous or [],
+                )
+                db.session.add(obj)
+            logger.info('Created new Flow %s', obj)
+        except SQLAlchemyError:
+            logger.exception(
+                'Failed to create Flow with %s, %s, %s, %s',
+                id_,
+                flow_id,
+                name,
+                payload,
+            )
+            raise
+        return obj
+
+    @classmethod
+    def get(cls, id_):
+        """Get a task object from the DB."""
+        return cls.query.get(id_)
+
+    def to_dict(self):
+        """Task dictionary representation."""
+        return {
+            'id': str(self.id),
+            'flow_id': str(self.flow_id),
+            'created': self.created.isoformat(),
+            'updated': self.updated.isoformat(),
+            'name': self.name,
+            'payload': self.payload,
+            'status': str(self.status),
+            'message': self.message,
+            'previous': self.previous,
+        }
---
+++ tests/unit/test_flows_api.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Document Server.
+# Copyright (C) 2020 CERN.
+#
+# CERN Document Server is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# CERN Document Server is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CERN Document Server; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+
+"""Python basic API tests."""
+
+from __future__ import absolute_import, print_function
+
+from cds.modules.flows import Flow, task
+from cds.modules.flows.models import Task
+
+
+def test_basic_flow_api_usage(db):
+    """Test basic flow creation."""
+
+    @task
+    def t1(common, **kwargs):
+        """Task with an important mission."""
+        # if common == 'common-arg':
+        #     raise Exception()
+        print('Run t1', common, kwargs)
+
+    @task
+    def t2(p, **kwargs):
+        """Task with an even more important mission."""
+        if p == 't2_1':
+            # This one doesn't actually run anything, we just return
+            return 'No need to run for {}'.format(p)
+
+        # Do important stuff when required
+        print('Run t2', p, kwargs)
+
+    @task
+    def t3(**kwargs):
+        """Task with a secret mission."""
+        print('Run t3', kwargs)
+
+    @task(bind=True, max_retries=None)
+    def t4(self, times, **kwargs):
+        """Run this one n times."""
+        print('Run t4: ', times, kwargs)
+        if times > 0:
+            self.commit_status(
+                kwargs['task_id'], message='Running for {}'.format(times)
+            )
+            # Testing message updates
+            t = Task.get(kwargs['task_id'])
+            assert t.status.value == 'PENDING'
+            assert t.message == 'Running for {}'.format(times)
+            f = Flow.get_flow(kwargs['flow_id'])
+            assert f.status['status'] == 'PENDING'
+
+            # Reschedule the task to mimic breaking long standing tasks
+            times = times - 1
+            self.retry(args=(times,), kwargs=kwargs)
+
+    flow = Flow.create('test', payload=dict(common='common-arg'))
+    assert flow.status['status'] == 'PENDING'
+
+    # build the workflow
+    def build(flow):
+        flow.chain(t1)
+        flow.group(t2, [{'p': 't2_1'}, {'p': 't2_2'}])
+        flow.chain(t3, {'p': 't3'})
+        flow.chain(t4, {'times': 10})
+
+    flow.assemble(build)
+
+    # Save tasks and flow before running
+    db.session.commit()
+
+    assert flow.status['status'] == 'PENDING'
+
+    flow.start()
+
+    flow = Flow.get_flow(flow.id)
+    assert flow.status['status'] == 'SUCCESS'
+
+    task_status = flow.status['tasks'][0]
+    assert task_status['status'] == 'SUCCESS'
+    flow_task_status = flow.get_task_status(task_status['id'])
+    assert flow_task_status['status'] == 'SUCCESS'
+
+    # Create a new instance of the same flow (restart)
+    old_flow_id = flow.id
+    flow = flow.__class__.create(
+        flow.name, payload={'common': 'test 2'}, previous_id=old_flow_id
+    )
+    assert flow.status['status'] == 'PENDING'
+    flow.assemble(build)
+
+    # Save tasks and flow before running
+    db.session.commit()
+
+    assert flow.status['status'] == 'PENDING'
+
+    flow.start()
+
+    assert flow.id != old_flow_id
+
+    flow = Flow.get_flow(flow.id)
+    assert flow.previous_id == old_flow_id
+
+    # Restart task
+    flow.restart_task(task_status['id'])
+    flow_task_status = flow.get_task_status(task_status['id'])
+    assert flow_task_status['status'] == 'SUCCESS'
