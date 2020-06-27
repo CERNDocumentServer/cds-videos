158e2e5e65184f8cff1f6d29b7e8e75da80185cd
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2020-06-27 17:19:08 -0400

    enhance live migration

--- cds/modules/flows/migration.py
+++ cds/modules/flows/migration.py
@@ -25,6 +25,7 @@
 
 """Flow migration helper functions."""
 
+from cds_sorenson.api import can_be_transcoded, get_all_distinct_qualities
 from invenio_db import db
 
 from ..deposit.api import deposit_video_resolver
@@ -44,9 +45,19 @@ def migrate_event(event):
     original_file = CDSVideosFilesIterator.get_master_video_file(deposit)
     has_metadata = 'extracted_metadata' in deposit.get('_cds', {})
     has_frames = bool(CDSVideosFilesIterator.get_video_frames(original_file))
-    presets_present = [
-        f.get('tags, {}').get('preset_quality', '')
-        for f in CDSVideosFilesIterator.get_video_subformats(original_file)
+    subformats = CDSVideosFilesIterator.get_video_subformats(original_file)
+    subformat_done = [
+        f.get('tags, {}').get('preset_quality', '') for f in subformats
+    ]
+    missing_subformats = [
+        s
+        for s in set(get_all_distinct_qualities()) - set(subformat_done)
+        if can_be_transcoded(
+            s,
+            original_file['tags']['display_aspect_ratio'],
+            int(original_file['tags']['width']),
+            int(original_file['tags']['height']),
+        )
     ]
 
     with db.session.begin_nested():
@@ -60,11 +71,14 @@ def migrate_event(event):
                     Status.SUCCESS if has_metadata else Status.PENDING
                 )
             elif 'TranscodeVideoTask' in task.name:
-                task.status = (
-                    Status.SUCCESS
-                    if task.payload.get('preset_quality') in presets_present
-                    else Status.PENDING
-                )
+                preset_quality = task.payload.get('preset_quality')
+                if preset_quality in missing_subformats:
+                    task.status = Status.FAILURE
+                elif preset_quality not in subformat_done:
+                    task.status = Status.SUCCESS
+                    task.message = 'Not transcoding for {}'.format(
+                        preset_quality
+                    )
             db.session.add(task)
     db.session.commit()
 
