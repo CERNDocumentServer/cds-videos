41fa821b25ee742897b2cb49e59b92260798afba
Nicola Tarocco <ntarocco@gmail.com>
2021-01-27 14:56:01 +0100

    global: fix tests

--- tests/unit/test_fixtures.py
+++ tests/unit/test_fixtures.py
@@ -124,7 +124,7 @@ def test_fixture_pages(app, script_info, db, client):
     res = runner.invoke(cli_pages, [], obj=script_info)
     assert res.exit_code == 0
     pages = Page.query.all()
-    assert len(pages) == 7
+    assert len(pages) == 5
     about_response = client.get('/about')
     assert about_response.status_code == 200
 
