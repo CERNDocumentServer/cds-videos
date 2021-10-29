fcc4d74abdeb491bd138c63dfbb4093cb707538e
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2021-10-29 11:30:43 +0200

    migration: create migration script for old videos

--- cds/modules/flows/migration.py
+++ cds/modules/flows/migration.py
@@ -26,34 +26,53 @@
 """Flow migration helper functions."""
 
 from flask import current_app
-from flask_login import current_user
 from invenio_db import db
+from invenio_files_rest.models import (
+    ObjectVersionTag,
+    as_object_version,
+)
+from invenio_pidstore.models import PersistentIdentifier, PIDStatus
+from cds.modules.records.utils import is_project_record
+from cds.modules.deposit.api import CDSDeposit
 
-from .api import Flow
-from ..deposit.api import deposit_video_resolver
 from ..opencast.utils import can_be_transcoded
 from ..records.api import CDSVideosFilesIterator
-from .models import Status
+from .api import Flow, uuid
+from .models import Status, TaskMetadata
 
 
-def migrate_event(deposit_id):
+def migrate_event(deposit):
     """Migrate an old event into Flows."""
     # Update flow task status depending on the content of th record
-    deposit = deposit_video_resolver(deposit_id)
+
+    deposit_id = deposit["_deposit"]["id"]
+    user_id = deposit["_deposit"]["created_by"]
 
     original_file = CDSVideosFilesIterator.get_master_video_file(deposit)
+    if not original_file:
+
+        return
     has_metadata = 'extracted_metadata' in deposit.get('_cds', {})
     has_frames = bool(CDSVideosFilesIterator.get_video_frames(original_file))
     subformats = CDSVideosFilesIterator.get_video_subformats(original_file)
+    payload = dict(
+        version_id=original_file["version_id"],
+        key=original_file["key"],
+        bucket_id=deposit['_buckets']['deposit'],
+        deposit_id=deposit_id
+    )
 
-    flow = Flow(deposit_id=deposit_id,
-                user_id=current_user,
-                payload=dict(
-                    version_id=original_file["version_id"],
-                    key=original_file["key"],
-                    bucket_id=deposit['_buckets']['deposit'],
-                    deposit_id=deposit_id
-                ))
+    flow = Flow(
+        deposit_id=deposit_id,
+        user_id=user_id,
+        payload=payload
+    )
+
+    # Create the object tag for _flow_id
+    object_version = as_object_version(original_file["version_id"])
+    ObjectVersionTag.create_or_update(
+            object_version, '_flow_id', str(flow.id)
+        )
 
     subformat_done = [
         f.get('tags', {}).get('preset_quality', '') for f in subformats
@@ -69,25 +88,78 @@ def migrate_event(deposit_id):
     ]
 
     with db.session.begin_nested():
-        for task in flow.model.tasks:
-            if 'DownloadTask' in task.name:
-                task.status = Status.SUCCESS
-            elif 'ExtractFramesTask' in task.name:
-                task.status = Status.SUCCESS if has_frames else Status.PENDING
-            elif 'ExtractMetadataTask' in task.name:
-                task.status = (
-                    Status.SUCCESS if has_metadata else Status.PENDING
-                )
-            elif 'TranscodeVideoTask' in task.name:
-                preset_quality = task.payload.get('preset_quality')
-                if preset_quality in missing_subformats:
-                    task.status = Status.FAILURE
-                elif preset_quality not in subformat_done:
-                    task.status = Status.SUCCESS
-                    task.message = 'Not transcoding for {}'.format(
-                        preset_quality
-                    )
-            db.session.add(task)
+        # add ExtractMetadataTask
+        task_id = uuid()
+        payload["flow_id"] = str(flow.id)
+
+        metadata_task = TaskMetadata.create(
+            id_=task_id,
+            flow_id=str(flow.id),
+            name='cds.modules.flows.tasks.ExtractMetadataTask',
+            previous=[],
+            payload=payload
+        )
+        metadata_task.status = Status.SUCCESS if has_metadata else Status.FAILURE
+        db.session.add(metadata_task)
+
+        # add ExtractFramesTask
+        task_id = uuid()
+
+        frames_task = TaskMetadata.create(
+            id_=task_id,
+            flow_id=str(flow.id),
+            name='cds.modules.flows.tasks.ExtractFramesTask',
+            previous=[],
+            payload=payload
+        )
+
+        frames_task.status=Status.SUCCESS if has_frames else Status.FAILURE
+        db.session.add(frames_task)
+
+        # add TranscodeVideoTask
+
+        for subformat in subformat_done:
+            task_id = uuid()
+            transcode_task = TaskMetadata.create(
+                id_=task_id,
+                flow_id=str(flow.id),
+                name='cds.modules.flows.tasks.TranscodeVideoTask',
+                previous=[],
+                payload=payload
+            )
+            transcode_task.status=Status.FAILURE if subformat in missing_subformats else Status.SUCCESS
+            db.session.add(transcode_task)
+
     db.session.commit()
 
     return flow
+
+
+def main():
+    """Migrate old video deposits to the new Flows."""
+
+    def get_all_pids_by(pid_type):
+        """Get all PIDs for the given type.
+
+        :param pid_type: String representing the PID type value, for example 'recid'.
+        """
+        pids = PersistentIdentifier.query.filter(PersistentIdentifier.pid_type == pid_type).filter(
+            PersistentIdentifier.status == PIDStatus.REGISTERED).all()
+        return pids
+
+    all_deps = get_all_pids_by("depid")
+    video_deps = []
+
+    for dep in all_deps:
+        rec = CDSDeposit.get_record(dep.object_uuid)
+        if not is_project_record(rec):
+            video_deps.append(rec)
+
+    for video in video_deps:
+        migrate_event(video)
+        # we need to commit to re-dump files/tags to the record
+        # and store `_flow_id`
+        video._update_tasks_status()
+        video['_files'] = video._get_files_dump()
+        video.commit()
+    db.session.commit()
