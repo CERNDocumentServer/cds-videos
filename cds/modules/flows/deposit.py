e0c9d5e141bc8ea9e4d9874424fe29ef2ae9fa7a
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2021-11-08 11:05:50 +0100

    deposit: fix state update if deposit is published before transcoding finishes

--- cds/modules/flows/deposit.py
+++ cds/modules/flows/deposit.py
@@ -12,6 +12,6 @@ def update_deposit_state(deposit_id=None):
     """Update deposit state on ElasticSearch."""
     from cds.modules.deposit.api import deposit_video_resolver
     if deposit_id:
-        video = deposit_video_resolver(deposit_id)
-        _index_deposit(video)
-        _index_deposit(video.project)
+        deposit_video = deposit_video_resolver(deposit_id)
+        _index_deposit(deposit_video)
+        _index_deposit(deposit_video.project)
