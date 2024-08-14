2fb185ecc8d1bd68844d1d3e820be5510f9e13a3
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2024-08-14 14:40:37 +0200

    deposit: fix trash icon disable condition

--- cds/modules/deposit/static/ckeditor/config.js
+++ cds/modules/deposit/static/ckeditor/config.js
@@ -3,42 +3,45 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-    // For complete reference see:
-    // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+CKEDITOR.editorConfig = function (config) {
+  // For complete reference see:
+  // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
-    config.skin = 'moono-lisa';
+  config.skin = "moono-lisa";
 
-    config.language = 'en';
+  config.language = "en";
 
-    // The toolbar groups arrangement, optimized for two toolbar rows.
-    config.toolbarGroups = [
-        { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-        { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-        { name: 'links' },
-        { name: 'insert' },
-        { name: 'forms' },
-        { name: 'tools' },
-        { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-        { name: 'others' },
-        '/',
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-        { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-        { name: 'styles' },
-        { name: 'colors' },
-        { name: 'about' }
-    ];
+  // The toolbar groups arrangement, optimized for two toolbar rows.
+  config.toolbarGroups = [
+    { name: "clipboard", groups: ["clipboard", "undo"] },
+    { name: "editing", groups: ["find", "selection", "spellchecker"] },
+    { name: "links" },
+    { name: "insert" },
+    { name: "forms" },
+    { name: "tools" },
+    { name: "document", groups: ["mode", "document", "doctools"] },
+    { name: "others" },
+    "/",
+    { name: "basicstyles", groups: ["basicstyles", "cleanup"] },
+    {
+      name: "paragraph",
+      groups: ["list", "indent", "blocks", "align", "bidi"],
+    },
+    { name: "styles" },
+    { name: "colors" },
+    { name: "about" },
+  ];
 
-    // Remove some buttons provided by the standard plugins, which are
-    // not needed in the Standard(s) toolbar.
-    config.removeButtons = 'Underline,Subscript,Superscript';
+  // Remove some buttons provided by the standard plugins, which are
+  // not needed in the Standard(s) toolbar.
+  config.removeButtons = "Underline,Subscript,Superscript";
 
-    // Set the most common block elements.
-    config.format_tags = 'p;h1;h2;h3;pre';
+  // Set the most common block elements.
+  config.format_tags = "p;h1;h2;h3;pre";
 
-    // Simplify the dialog windows.
-    config.removeDialogTabs = 'image:advanced;link:advanced';
+  // Simplify the dialog windows.
+  config.removeDialogTabs = "image:advanced;link:advanced";
 
-    // Remove some plugins
-    config.removePlugins = 'maximize';
+  // Remove some plugins
+  config.removePlugins = "maximize";
 };
